bd3510732bdcc70aaca3c1f60535b3994d6151e3
Simon Weis <laguna.ws@gmail.com>
2023-10-22 10:39:00 +0200

    Add test case for includin/excluding invisible layers

--- test/unit_tests/make_image_from_ase_test.cpp
+++ test/unit_tests/make_image_from_ase_test.cpp
@@ -1,3 +1,4 @@
+#include "aselib/constants.hpp"
 #include "catch2/catch.hpp"
 #include <aselib/aseprite_data.hpp>
 #include <aselib/image_builder.hpp>
@@ -25,14 +26,36 @@ TEST_CASE("make image from ase with one layer", "[image]")
 
 TEST_CASE("make image from ase with multiple layers", "[image]")
 {
-    AsepriteData const ase { "assets/test/unit/miner.aseprite" };
+    AsepriteData ase { "assets/test/unit/miner.aseprite" };
 
     auto const img = makeImageFromAse(ase);
 
     REQUIRE(img.m_width == 320);
     REQUIRE(img.m_height == 32);
 
+    REQUIRE(img.getPixelAt(16, 24) == PixelDataRGBA { 248, 192, 144, 255 });
+    REQUIRE(img.getPixelAt(14, 15) == PixelDataRGBA { 43, 41, 40, 255 });
+}
+
+TEST_CASE("make image from ase with multiple layers and invisible layer", "[image]")
+{
+    AsepriteData ase { "assets/test/unit/miner.aseprite" };
+
+    for (auto& layer : ase.m_frames[0].m_chunks.m_layers_chunks) {
+        if (layer.m_layer_name == "outline") {
+            layer.m_layer_flags
+                = aselib::layer_flags::editable | aselib::layer_flags::lock_movement;
+            break;
+        }
+    }
+
+    auto const img = makeImageFromAse(ase, false);
+
+    REQUIRE(img.m_width == 320);
+    REQUIRE(img.m_height == 32);
+
     REQUIRE(img.m_pixels[img.posToIndex(16, 24)] == PixelDataRGBA { 248, 192, 144, 255 });
+    REQUIRE(img.m_pixels[img.posToIndex(14, 15)] == PixelDataRGBA { 0, 0, 0, 0 });
 }
 
 TEST_CASE("make image from ase with two layers and opacity", "[image]")
