c0c91ec40ec0c63f6b74ddf82da4c8f814cd9d4d
Simon Weis <laguna.ws@gmail.com>
2024-07-07 11:27:51 +0200

    Improve error message

--- impl/aselib/image.cpp
+++ impl/aselib/image.cpp
@@ -5,8 +5,8 @@
 std::size_t aselib::Image::posToIndex(std::uint32_t x, std::uint32_t y) const
 {
     if (x >= m_width || y >= m_height) [[unlikely]] {
-        throw std::invalid_argument { "invalid pixel position in image: " + std::to_string(x) + ", "
-            + std::to_string(y) };
+        throw std::invalid_argument { "invalid pixel position (" + std::to_string(x) + ", "
+            + std::to_string(y) + ") in image with size ("  + std::to_string(m_width) + ", " + std::to_string(m_height) + ")" };
     }
     return y * m_width + x;
 }
